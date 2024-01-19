48e6794ed0995fa29b0598f7d8c08c39b2c4408c
Willian Pereira Almeida <willianpereiraalmeida@hotmail.com>
2024-01-19 18:53:47 +0000

    applying another version of the application

--- app.py
+++ app.py
@@ -12,16 +12,12 @@ def obter_taxa_de_cambio(base_currency, target_currency):
     if response.status_code != 200:
         return None, f"Erro na requisição à API: {response.status_code}"
 
-    dados = response.json()
-
-    if 'rates' not in dados:
-        return None, "Resposta da API não contém a chave 'rates'"
-
-    taxa = dados['rates'][target_currency]
-    return taxa, None
-
-def converter_moeda(valor, taxa):
-    return valor * taxa
+    try:
+        dados = response.json()
+        taxa = dados['rates'][target_currency]
+        return taxa, None
+    except KeyError:
+        return None, f"A resposta da API não contém a taxa para a moeda de destino '{target_currency}'"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
